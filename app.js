0888efa9510a3bb8b965f78af24234b623c69fec
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-14 23:02:54 -0300

    background image changes with click

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ morty.play();
 
 window.addEventListener('keydown', function(e){
   const balls = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const image = document.querySelector(`[data-key='${e.keyCode}']`);
   if (!balls) return;
   //every time key is pressed, restart audio file to beginning.
   //otherwise, js waits until audio clip is done playing to restart.
   balls.currentTime = 0;
   balls.play();
-  //console.log(audio);
+  image.classList.add('pressed');
+  //console.log(image);
 });
