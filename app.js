f582d357e78b1aca4ec796fa0d233236385d112a
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-15 00:04:52 -0300

    changed to custom font. added link to restart which only appears after spacebar is pressed.

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ morty.play();
 window.addEventListener('keydown', function(e){
   const balls = document.querySelector(`audio[data-key='${e.keyCode}']`);
   const image = document.querySelector(`[data-key='${e.keyCode}']`);
+  const button = document.querySelector(".button-before");
   if (!balls) return;
   //every time key is pressed, restart audio file to beginning.
   //otherwise, js waits until audio clip is done playing to restart.
   balls.currentTime = 0;
   balls.play();
   image.classList.add('pressed');
+  button.classList.add('button-after');
   //console.log(image);
 });
